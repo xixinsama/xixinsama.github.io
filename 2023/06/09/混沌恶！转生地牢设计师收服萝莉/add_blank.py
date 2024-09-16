bb0bba1022eed72a8fae2b5585564087ad07ec02
xixinsama <xixin142857@ooutlook.com>
2024-09-16 15:41:22 +0800

    Site updated: 2024-09-16 15:41:19

---
+++ 2023/06/09/混沌恶！转生地牢设计师收服萝莉/add_blank.py
@@ -0,0 +1,15 @@
+input_file = '混沌恶！转生地牢设计师收服萝莉.md'
+output_file = 'output_mdfile.md'
+
+with open(input_file, 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+# 在每行末尾添加两个空格
+with open(output_file, 'w', encoding='utf-8') as file:
+    for line in lines:
+        # 如果是标题行，不添加空格
+        # 如果是空行，不添加空格
+        if line.startswith('#') or line.startswith('\n'):
+            file.write(line)
+        else:
+            file.write(line.rstrip() + '  \n')
